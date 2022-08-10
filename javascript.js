3ea94831bd08762aeac8b96a610d3d9814ce1ff8
Soulstrike <o.patrick1985@gmail.com>
2022-08-10 23:49:43 +0200

    add func to compare comp and player and get winner

--- javascript.js
+++ javascript.js
@@ -1,26 +1,62 @@
 const computerChoice = getComputerChoice();
 const playerChoice = getPlayerChoice();
+let roundWon = playRound(computerChoice, playerChoice);
 
-function getComputerChoice() {
-    const randomComp = Math.floor(Math.random() * 3);
+function getComputerChoice() { 
+    const randomComp = Math.floor(Math.random() * 3); // get random number between 0-2, each number equals one choice
     if (randomComp === 0) {
-        return "Stone";
+        return "rock";
     } else if (randomComp === 1) {
-        return "Scissor";            
+        return "scissor";            
     } else {
-        return "Paper";
+        return "paper";
     }
 }
 function getPlayerChoice() {
-    const playerInput = prompt("Choose your Weapon! Will it be Rock, Paper or Scissor?").toLowerCase();
-    console.log(playerInput);
-    if (playerInput === "rock") {
-        return "Rock";
+    const playerInput = prompt("Choose your Weapon! Will it be Rock, Paper or Scissor?").toLowerCase(); // user input
+    if (playerInput === "rock") { // compare user input and return choice
+        return "rock";
     } else if (playerInput === "scissor") {
-        return "Scissor";
+        return "scissor";
     } else if(playerInput === "paper") {
-        return "Paper";
+        return "paper";
     } else {
-        return "Choose Rock, Paper or Scissor only!"
+        return "Choose Rock, Paper or Scissor only!" // Error - faulty input
     }
 }
+function playRound(computerChoice, playerChoice) {
+    if (computerChoice === "rock") {
+        switch (playerChoice) {
+            case "paper":
+                return "You win! Paper beats rock.";
+            case "scissor":
+                return "You lose! rock crushes Scissor";
+            case "rock":
+            default:
+                return "Draw! Try again.";
+        }
+    } else if (computerChoice === "paper") {
+        switch (playerChoice) {
+            case "scissor":
+                return "You win! Scissor cuts Paper.";
+            case "rock":
+                return "You lose! Paper wraps Rock.";
+            case "paper":
+            default: 
+                return "Draw! Try again.";
+        }
+    } else if (computerChoice === "scissor") {
+        switch (playerChoice) {
+            case "rock":
+                return "You win! Rock crushes Scissor.";
+            case "paper":
+                return "You lose! Scissor cuts paper.";
+            case "scissor":
+            default: 
+                return "Draw! Try again.";
+        }
+    }
+}
+console.log(computerChoice);
+console.log(playerChoice);
+console.log(roundWon);
