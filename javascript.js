8657e076f4bec9fb4b7f58f8d82ecc3162df47c1
Soulstrike <o.patrick1985@gmail.com>
2022-08-11 22:04:07 +0200

    fix game loop(return values)

--- javascript.js
+++ javascript.js
@@ -1,72 +1,71 @@
-let computerChoice = getComputerChoice();
-let playerChoice = getPlayerChoice();
-let roundWon = playRound(computerChoice, playerChoice);
+let computerChoice = "";
+let playerChoice = "Choose Rock";
+let roundWon = "";
 
 function getComputerChoice() { 
     const randomComp = Math.floor(Math.random() * 3); // get random number between 0-2, each number equals one choice
     if (randomComp === 0) {
-        return "rock";
+        return computerChoice = "rock";
     } else if (randomComp === 1) {
-        return "scissor";            
+        return computerChoice = "scissor";            
     } else {
-        return "paper";
+        return computerChoice = "paper";
     }
 }
 function getPlayerChoice() {
-    let playerInput = "Choose Rock";
-    while (playerInput.startsWith("Choose Rock")) {
-        playerInput = prompt("Choose your Weapon! Will it be Rock, Paper or Scissor?").toLowerCase(); // user input
-        if (playerInput === "rock") { // compare user input and return choice
-        return "rock";
-        } else if (playerInput === "scissor") {
-        return "scissor";
-        } else if(playerInput === "paper") {
-        return "paper";
-        } else {
-        return "Choose Rock, Paper or Scissor only!" // Error - faulty input
-        }
+    const playerInput = prompt("Choose your Weapon! Will it be Rock, Paper or Scissor?").toLowerCase(); // user input
+    if (playerInput === "rock") { // compare user input and return choice
+    return playerChoice = "rock";
+    } else if (playerInput === "scissor") {
+    return playerChoice = "scissor";
+    } else if(playerInput === "paper") {
+    return playerChoice = "paper";
+    } else {
+    return playerChoice = "Choose Rock, Paper or Scissor only!" // Error - faulty input
     }
 }
 function playRound(computerChoice, playerChoice) {
     if (computerChoice === "rock") {
         switch (playerChoice) {
             case "paper":
-                return "You win! Paper beats rock.";
+                return roundWon = "You win! Paper beats rock.";
             case "scissor":
-                return "You lose! rock crushes Scissor";
+                return roundWon = "You lose! rock crushes Scissor";
             case "rock":
             default:
-                return "Draw! Try again.";
+                return roundWon = "Draw! Try again.";
         }
     } else if (computerChoice === "paper") {
         switch (playerChoice) {
             case "scissor":
-                return "You win! Scissor cuts Paper.";
+                return roundWon = "You win! Scissor cuts Paper.";
             case "rock":
-                return "You lose! Paper wraps Rock.";
+                return roundWon = "You lose! Paper wraps Rock.";
             case "paper":
             default: 
-                return "Draw! Try again.";
+                return roundWon = "Draw! Try again.";
         }
     } else if (computerChoice === "scissor") {
         switch (playerChoice) {
             case "rock":
-                return "You win! Rock crushes Scissor.";
+                return roundWon = "You win! Rock crushes Scissor.";
             case "paper":
-                return "You lose! Scissor cuts paper.";
+                return roundWon = "You lose! Scissor cuts paper.";
             case "scissor":
             default: 
-                return "Draw! Try again.";
+                return roundWon = "Draw! Try again.";
         }
     }
 }
-/* function game() {
-    let compWins = 0;
-    let playerWins = 0;
-    let draw = 0;
-    for (let i = 0; i < 5; i++) {
+function game() {
+    let compWins = 0; // set computer wins to zero
+    let playerWins = 0; // set player wins to zero
+    let draw = 0; // set draw matches to zero
+    for (let i = 0; i < 5; i++) { // loop the game 5 times
         getComputerChoice();
+        do {
         getPlayerChoice();
+        } while (playerChoice.startsWith("Choose Rock"));
         playRound(computerChoice, playerChoice);
         if (roundWon.startsWith("You win!")) {
             playerWins++;
@@ -78,7 +77,7 @@ function playRound(computerChoice, playerChoice) {
         console.log("Won: " + playerWins + " Lost: " + compWins + " Draw: " + draw);
     }
 }
-game(); */
-console.log(computerChoice);
+game(); 
+/* console.log(computerChoice);
 console.log(playerChoice);
-console.log(roundWon);
+console.log(roundWon); */
